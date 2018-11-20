e00191466dcce389fc5e71c9e82692c2463973f5
rezonjov <rezonjov@gmail.com>
2018-11-20 12:03:44 +0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'typed-angular';
+
+  ngOnInit() {
+    // const greeting = [
+    //   'is functionally creative',
+    //   'hunts down bugs',
+    //   'snacks a lot',
+    //   'keeps succulents as pets',
+    // ];
+    // const options = {
+    //   strings: greeting,
+    //   typeSpeed: 100,
+    //   backSpeed: 100,
+    //   startDelay: 500,
+    //   backDelay: 4500,
+    //   loop: true,
+    // };
+    //  const typed = new Typed('.typed-text', options);
+    //  typed.start();
+  }
 }
